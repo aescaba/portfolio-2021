3e27a7c7aa96e241e9075b88c55f06194a26d4dd
Amanda Escaba <amandaescaba@gmail.com>
2021-01-04 02:01:36 -0500

    added disclaimer to hero

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -58,6 +58,10 @@ const StyledContentWrapper = styled(ContentWrapper)`
       font-size: 1.125rem;
       margin-bottom: 2rem;
     }
+    blockquote {
+      color: #F4C416;
+      font-weight:200;
+    }
   }
 `
 
